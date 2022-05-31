5447f3b19b07798ee54915dc0216b36348a78d68
unknown <kaitlandevans@gmail.com>
2022-05-30 20:14:27 -0500

    fetch created

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 console.clear();
 
 const list = document.querySelector("#list");
-fetch("https://randomuser.me/api/")
+fetch("https://randomuser.me/api/?results20")
   .then((response) => response.json())
   .then((data) => {
     let fetchedData = data.results;
+  }
+console.log(fetchedData)
